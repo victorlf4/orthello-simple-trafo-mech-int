759dd3e1c8bfbb636defe3872a70cc2cf32dcfee
jon <jonathan.n@u.nus.edu>
2023-01-21 19:22:45 -0800

    streamlit init

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.text_input("Clean Prompt", key="clean_prompt")
+
+st.text_input("Correct Answer", key="correct_answer")
+
+st.button("Run in gelu-2l", key="run_gelu_2l")
+
+st.text_input("Clean Prompt", key="clean_prompt")
+
+st.text_input("Correct Answer", key="correct_answer")
+
+st.button("Run test_propmpt", key="run_test_prompt")
+
